b64d06313ed304c363ba2bf6a2647090c182c0be
James Putnam <jonathan.putnam86@gmail.com>
2016-11-01 17:20:28 -0600

    moreStuff

---
+++ forLoop.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main(){
+
+  i:= 0
+
+  for i <=10 {
+
+    fmt.PrintIn(i)
+
+    i++
+  }
+
+
+}
+//can also iterate all on one line like JS
+func main(){
+  for j:=0; j < 8; j++{
+    fmt.Println(j)
+  }
+}
